c55ed5a8c4b1de9a6b800d7149fb3753643a33e8
03620505039 <03620505039@senacrs.edu.br>
2024-06-20 21:48:48 -0300

    potato

--- aula 3/sistema/admin/cadastroCliente.php
+++ aula 3/sistema/admin/cadastroCliente.php
@@ -16,6 +16,104 @@
   <?php require "includes/menu.php";?>
   <h1>Cadastro Baitola</h1>
 
+<form method="post" action="acao/acaoCliente.php">
+
+<h3 style="margin-top: 30px; color:gray">Dados Pessoais</h3>
+<div class="row">
+  <div class="col-md-12">
+    <div class="form-group">
+    <label for="exampleInputEmail1">Nome</label>
+    <input type="text" name="nome" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Seu nome">
+    </div>
+  </div>
+</div>
+
+<div class="row">
+    <div class="col-md-4">
+      <div class="form-group">
+      <label for="exampleInputEmail1">Cpf</label>
+      <input type="text" name="cpf" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="000.000.000-00">
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="form-group">
+      <label for="exampleInputEmail1">Data de nascimento<data value=""></data></label>
+      <input type="date" name="data" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="dd/mm/yy">
+      </div>
+    </div>
+    <div class="col-md-4">sexo
+          <div class="form-check">
+        <input class="form-check-input" type="radio" name="radios"  value="Masculino">
+        <label class="form-check-label" for="exampleRadios1">
+          Masculino
+        </label>
+      </div>
+      <div class="form-check">
+        <input class="form-check-input" type="radio" name="radios"  value="Feminino">
+        <label class="form-check-label" for="exampleRadios2">
+          Feminino
+        </label>
+      </div>
+    </div>
+</div>
+
+  <div class="row">
+  <div class="col-md-4">estado civil
+  <select class="form-control" name="ec" style="margin-top: 5px;">
+  <option>solteiro</option>
+  <option>casado</option>
+  <option>mamando</option>
+</select>
+  </div>
+    <div class="col-md-4">
+      <div class="form-group">
+      <label for="exampleInputEmail1">Renda Mensal</label>
+      <input type="text" name="renda" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="0,00">
+      </div>
+    </div>  
+  </div>
+
+<h3 style="margin-top: 30px; color:gray">Endereço</h3>
+  <div class="row">
+  <div class="col-md-6">
+  <div class="form-group">
+      <label for="exampleInputEmail1">Logradouro</label>
+      <input type="text" name="logradouro" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="av.pau">
+      </div>
+  </div>
+    <div class="col-md-3">
+    <div class="form-group">
+      <label for="exampleInputEmail1">Número</label>
+      <input type="text" name="numero" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="ñ">
+      </div>
+    </div>
+    <div class="col-md-3">
+    <div class="form-group">
+      <label for="exampleInputEmail1">Complemento</label>
+      <input type="text" name="complemento" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Casa, Apto, Loja">
+      </div>
+    </div>
+  </div>
+
+  <div class="row">
+  <div class="col-md-6">estado
+  <select class="form-control" name="estado" style="margin-top: 5px;">
+  <option>Acre</option>
+  <option>Bahia</option>
+  <option>Ceara</option>
+</select>
+  </div>
+    <div class="col-md-6">
+    <div class="form-group">
+      <label for="exampleInputEmail1">Cidade</label>
+      <input type="text" name="cidade" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="São Paulo">
+    </div>
+    </div>
+    <input type="submit" value="Enviar" name="enviar" class="btn btn-dark" style="margin-left: 15px;">
+  </div>
+</form>
+
+
   <?php require "includes/rodape.php";?>
 </div>
     <!-- JavaScript (Opcional) -->
